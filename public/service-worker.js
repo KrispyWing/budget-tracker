bc7cf4014bac6348aeb87ef6927879c6cd6fcc6f
Kris Pennimpede <kpennimpede@hotmail.com>
2021-04-17 22:01:44 -0400

    add manifest.json to allow for installation

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,6 +5,7 @@ const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
+  "/manifest.json",
   "/css/styles.css",
   "/icons/icon-72x72.png",
   "/icons/icon-96x96.png",
